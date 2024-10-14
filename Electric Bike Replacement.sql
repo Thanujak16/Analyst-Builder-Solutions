882fdf47408d91980076c93b621c851930e3f446
Thanujak16 <119279936+Thanujak16@users.noreply.github.com>
2024-10-14 16:53:37 +0530

    Create Electric Bike Replacement.sql

---
+++ Electric Bike Replacement.sql
@@ -0,0 +1,7 @@
+/*After about 10,000 miles, Electric bike batteries begin to degrade and need to be replaced.
+
+Write a query to determine the amount of bikes that currently need to be replaced.*/
+
+
+SELECT COUNT(bike_id) FROM bikes 
+WHERE miles> 10000;
