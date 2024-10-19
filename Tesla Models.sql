ade19e665858b7138f180961870cca365a7f082e
Thanujak16 <119279936+Thanujak16@users.noreply.github.com>
2024-10-19 21:06:06 +0530

    Create Tesla Models.sql

---
+++ Tesla Models.sql
@@ -0,0 +1,10 @@
+#Tesla just provided their quarterly sales for their major vehicles.
+
+#Determine which Tesla Model has made the most profit.
+
+#Include all columns with the "profit" column at the end.
+
+SELECT tesla_model,car_price,cars_sold,production_cost, ((car_price-production_cost)*cars_sold) AS profit 
+FROM tesla_models 
+ORDER BY ((car_price-production_cost)*cars_sold) DESC
+LIMIT 1;
