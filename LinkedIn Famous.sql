8609c69c18f9cbf8f07f74db97f0e0f7f4e492ef
Thanujak16 <119279936+Thanujak16@users.noreply.github.com>
2024-10-19 16:13:29 +0530

    Create LinkedIn Famous.sql

---
+++ LinkedIn Famous.sql
@@ -0,0 +1,14 @@
+### Write a query to determine the popularity of a post on LinkedIn
+
+Popularity is defined by number of actions (likes, comments, shares, etc.) divided by the number impressions the post received * 100.
+
+If the post receives a score higher than 1 it was very popular.
+
+Return all the post IDs and their popularity where the score is 1 or greater.
+
+Order popularity from highest to lowest. ###
+
+SELECT post_id, (actions/impressions)*100 AS popularity 
+FROM linkedin_posts 
+WHERE (actions/impressions)*100 >=1
+ORDER BY popularity DESC;
